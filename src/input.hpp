d25051363f75da0b2ebd602f89c6043abd18432e
ere erer <sjorsmallon@gmail.com>
2024-10-14 20:06:07 +0200

    started on input event handling.

---
+++ src/input.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+enum class Key
+{
+	KEY_PLACEHOLDER = 0,
+	KEY_W,
+	KEY_ESCAPE,
+	KEY_A,
+	KEY_S,
+	KEY_D,
+	KEY_SPACE,
+};
+
+enum class Mouse
+{
+	MOUSE_LEFT = 0,
+	MOUSE_RIGHT,
+	MOUSE_SCROLL,
+	MOUSE_FORWARD,
+	MOUSE_BACKWARD
+};
--- tst/SDL_test.cc
+++ tst/SDL_test.cc
@@ -3,36 +3,88 @@
 #include <glad/glad.h>
 #include <print>
 
+
+#include "../src/input.hpp"
+
 #define SDL_MAIN_HANDLED
 
-//TODO: figure out why this window completely detaches from the console?
+// this is to abstract from SDL keypresses but it is kind of ugly actually (the pavel trick.)
+static Key to_key(SDL_Event& event)
+{
+    static constexpr auto mapping = std::initializer_list<std::pair<uint32_t, Key>>{
+        {SDLK_W, Key::KEY_W},
+        {SDLK_ESCAPE, Key::KEY_ESCAPE},
+        {SDLK_A ,Key::KEY_A},
+        {SDLK_S,Key::KEY_S},
+        {SDLK_D,Key::KEY_D},
+    };
+
+    for (auto& [key, value]: mapping)
+    {
+        if (key == event.key.key) // key.key???? zowie wowie!
+        {
+            return value;
+        }
+    }
+
+    return Key::KEY_PLACEHOLDER;
+}
+
+static Mouse to_mouse(SDL_Event& event)
+{
+
+}
+
+void handle_keyboard_input(Key key)
+{
+    switch (key)
+    {
+        case Key::KEY_ESCAPE:
+        {
+            std::print("pressed Escape. See you!\n");
+            exit(0);    
+            break;
+        }
+
+        case Key::KEY_PLACEHOLDER:
+        {
+            std::print("pressed key that I do not know.\n");
+            break;
+        }
+
+        default:
+        {
+            std::print("unreachable.\n");
+            break;            
+        }
+
+    }
+}
 
 // argc and argv[] are necessary for SDL3 main compatibility trickery.
 int main(int argc, char *argv[])
 {
 	static SDL_Window* window = nullptr;
-    SDL_SetAppMetadata("tremble", "1.0", "com.example.renderer-clear");
 
+    SDL_SetAppMetadata("tremble", "1.0", "com.example.renderer-clear");
 
     // Window mode MUST include SDL_WINDOW_OPENGL for use with OpenGL.
     window = SDL_CreateWindow(
-        "SDL3/OpenGL Demo", 640, 480, 
+        "SDL3/OpenGL Demo", 1920, 1080, 
         SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
   
-    SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-    SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 5);
-    SDL_GL_SetAttribute( SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
     // Create an OpenGL context associated with the window.
     SDL_GLContext gl_context = SDL_GL_CreateContext(window);
-
-    // do some fuckery with glad to actually load modern opengl.
+    // do some trickery with glad to actually load modern opengl.
     gladLoadGL();
 
-	// Main loop flag
+
     bool running = true;
 
-    // Event structure
     SDL_Event event;
 
     // Main loop
@@ -42,6 +94,13 @@ int main(int argc, char *argv[])
         // Process events
         while (SDL_PollEvent(&event))
         {
+
+            if (event.type == SDL_EVENT_KEY_DOWN)
+            {
+                auto key = to_key(event);
+                handle_keyboard_input(key);
+            }
+
             if (event.type == SDL_EVENT_QUIT)
             {
                 running = false;
@@ -52,12 +111,7 @@ int main(int argc, char *argv[])
         glClearColor(1.0f,0,0,1);
         glClear(GL_COLOR_BUFFER_BIT);
 
-
         // Your rendering code goes here (e.g., drawing shapes, textures, etc.)
-
-
-
-
 
 
         // Present the current buffer to the screen
